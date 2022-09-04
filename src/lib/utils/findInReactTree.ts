3b8df42ce36bfb6c06154f372db9cda18303fff6
Beef <beefers@riseup.net>
2022-09-04 22:09:55 +0100

    [Utils > findInReactTree] Actually return, how did I miss this?

--- src/lib/utils/findInReactTree.ts
+++ src/lib/utils/findInReactTree.ts
@@ -2,7 +2,7 @@ import findInTree from "@utils/findInTree";
 import { SearchFilter } from "@/def";
 
 export default function findInReactTree(tree: { [key: string]: any }, filter: SearchFilter): any {
-    findInTree(tree, filter, {
+    return findInTree(tree, filter, {
         walkable: ["props", "children", "child", "sibling"],
     });
 }
