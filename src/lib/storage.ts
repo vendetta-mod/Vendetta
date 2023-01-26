6eaf6ad283a3ff16ae1abf3260587213aa120f98
redstonekasi <kasi@hanneskann.de>
2023-01-26 23:49:41 +0100

    [storage] add useProxy hook

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -4,14 +4,19 @@ import createEmitter from "./emitter";
 
 const MMKVManager = RN.NativeModules.MMKVManager as MMKVManager;
 
+const emitterSymbol = Symbol("emitter accessor");
+
 export function createProxy(target: any = {}): { proxy: any, emitter: Emitter } {
   const emitter = createEmitter();
 
   function createProxy(target: any, path: string[]): any {
     return new Proxy(target, {
       get(target, prop: string) {
+        if ((prop as unknown) === emitterSymbol)
+          return emitter;
+
         const newPath = [...path, prop];
-        const value = target[prop];
+        const value: any = target[prop];
 
         if (value !== undefined && value !== null) {
           emitter.emit("GET", {
@@ -51,6 +56,26 @@ export function createProxy(target: any = {}): { proxy: any, emitter: Emitter } 
     proxy: createProxy(target, []),
     emitter,
   }
+}
+
+export function useProxy<T>(storage: T): T {
+  const emitter = (storage as any)[emitterSymbol] as Emitter;
+
+  const [, forceUpdate] = React.useReducer((n) => ~n, 0);
+
+  React.useEffect(() => {
+    const listener = () => forceUpdate();
+
+    emitter.on("SET", listener);
+    emitter.on("DEL", listener);
+
+    return () => {
+      emitter.off("SET", listener);
+      emitter.off("DEL", listener);
+    }
+  }, [])
+
+  return storage;
 }
 
 export async function createStorage<T>(storeName: string): Promise<Awaited<T>> {
