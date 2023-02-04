76ea19f8efc19c509abd41b6250ff87c90516a02
Beef <beefers@riseup.net>
2023-02-04 02:28:08 +0000

    [Storage] Run prettier

--- src/lib/emitter.ts
+++ src/lib/emitter.ts
@@ -1,35 +1,33 @@
 import { Emitter, EmitterEvent, EmitterListener, EmitterListenerData, EmitterListeners } from "@types";
 
 export const Events = Object.freeze({
-  GET: "GET",
-  SET: "SET",
-  DEL: "DEL"
+    GET: "GET",
+    SET: "SET",
+    DEL: "DEL",
 });
 
 export default function createEmitter(): Emitter {
-  return {
-    listeners: Object.values(Events).reduce<EmitterListeners>((acc, val: string) => ((acc[val] = new Set<EmitterListener>()), acc), {}) as EmitterListeners,
+    return {
+        listeners: Object.values(Events).reduce<EmitterListeners>((acc, val: string) => ((acc[val] = new Set<EmitterListener>()), acc), {}) as EmitterListeners,
 
-    on(event: EmitterEvent, listener: EmitterListener) {
-      if (!this.listeners[event].has(listener))
-        this.listeners[event].add(listener);
-    },
+        on(event: EmitterEvent, listener: EmitterListener) {
+            if (!this.listeners[event].has(listener)) this.listeners[event].add(listener);
+        },
 
-    off(event: EmitterEvent, listener: EmitterListener) {
-      this.listeners[event].delete(listener);
-    },
+        off(event: EmitterEvent, listener: EmitterListener) {
+            this.listeners[event].delete(listener);
+        },
 
-    once(event: EmitterEvent, listener: EmitterListener) {
-      const once = (event: EmitterEvent, data: EmitterListenerData) => {
-        this.off(event, once);
-        listener(event, data);
-      }
-      this.on(event, once);
-    },
+        once(event: EmitterEvent, listener: EmitterListener) {
+            const once = (event: EmitterEvent, data: EmitterListenerData) => {
+                this.off(event, once);
+                listener(event, data);
+            };
+            this.on(event, once);
+        },
 
-    emit(event: EmitterEvent, data: EmitterListenerData) {
-      for (const listener of this.listeners[event])
-        listener(event, data);
-    },
-  };
+        emit(event: EmitterEvent, data: EmitterListenerData) {
+            for (const listener of this.listeners[event]) listener(event, data);
+        },
+    };
 }
