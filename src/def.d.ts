16c881d2f218b589f7915c60b17a6c2e4100c272
redstonekasi <kasi@hanneskann.de>
2023-02-05 15:59:42 +0100

    [TS] Add definition for DCDFileManager

--- src/def.d.ts
+++ src/def.d.ts
@@ -176,6 +176,42 @@ interface MMKVManager {
     clear: () => void;
 }
 
+interface DCDFileManager {
+    /**
+     * @param path **Full** path to file
+     */
+    fileExists: (path: string) => Promise<boolean>;
+    /**
+     * Allowed URI schemes on Android: `file://`, `content://` ([See here](https://developer.android.com/reference/android/content/ContentResolver#accepts-the-following-uri-schemes:_3))
+     */
+    getSize: (uri: string) => Promise<boolean>;
+    /**
+     * @param path **Full** path to file
+     * @param encoding Set to `base64` in order to encode response
+     */
+    readFile(path: string, encoding: "base64" | "utf8"): Promise<string>;
+    /**
+     * Beware! This function has differing functionality on IOS and Android.
+     * @param storageDir Either `cache` or `documents`.
+     * @param path Path in `storageDir`, parents are recursively created.
+     * @param data The data to write to the file
+     * @param encoding Set to `base64` if `data` is base64 encoded.
+     * @returns Promise that resolves to path of the file once it got written
+     */
+    writeFile(storageDir: "cache" | "documents", path: string, data: string, encoding: "base64" | "utf8"): Promise<string>;
+    getConstants: () => {
+        /**
+         * The path the `documents` storage dir (see {@link writeFile}) represents.
+         */
+        DocumentsDirPath: string;
+    };
+    /**
+     * Will apparently cease to exist some time in the future so please use {@link getConstants} instead.
+     * @deprecated
+     */
+    DocumentsDirPath: string;
+}
+
 type Indexable<Type> = { [index: string]: Type }
 
 type EmitterEvent = "SET" | "GET" | "DEL";
