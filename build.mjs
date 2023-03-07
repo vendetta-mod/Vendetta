3531132c381811dfcfc8d98402747b6aafeefb0d
redstonekasi <kasi@hanneskann.de>
2023-03-07 01:23:16 +0100

    [Build] Fix ErrorBoundary exporting (#30)

--- build.mjs
+++ build.mjs
@@ -1,47 +1,54 @@
+import { build } from "esbuild";
+import { replace } from "esbuild-plugin-replace";
+import alias from "esbuild-plugin-alias";
+import swc from "@swc/core";
 import { promisify } from "util";
 import { exec as _exec } from "child_process";
 import fs from "fs/promises";
-
-import { rollup } from "rollup";
-import { swc } from "rollup-plugin-swc3";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
-import esbuild from "rollup-plugin-esbuild";
-import replace from "@rollup/plugin-replace";
-import nodeResolve from "@rollup/plugin-node-resolve";
-
+import path from "path";
 const exec = promisify(_exec);
+
+const tsconfig = JSON.parse(await fs.readFile("./tsconfig.json"));
+const aliases = Object.fromEntries(Object.entries(tsconfig.compilerOptions.paths).map(([alias, [target]]) => [alias, path.resolve(target)]));
 const commit = (await exec("git rev-parse HEAD")).stdout.trim().substring(0, 7) || "custom";
 
 try {
-    const bundle = await rollup({
-        input: "src/index.ts",
-        onwarn: () => {},
-        plugins: [
-            replace({
-                __vendettaVersion: commit,
-                preventAssignment: true,
-            }),
-            typescriptPaths(),
-            nodeResolve({ extensions: [".tsx", ".ts", ".jsx", ".js", ".json"] }),
-            swc({
-                env: {
-                    targets: "defaults",
-                    include: [
-                        "transform-classes",
-                        "transform-arrow-functions",
-                    ],
-                },
-            }),
-            esbuild({ minify: true }),
-        ],
-    });
-
-    await bundle.write({
+    await build({
+        entryPoints: ["./src/index.ts"],
+        outfile: "./dist/vendetta.js",
+        minify: true,
+        bundle: true,
         format: "iife",
-        file: "dist/vendetta.js",
-        compact: true,
+        target: "esnext",
+        plugins: [
+            {
+                name: "swc",
+                setup: (build) => {
+                    build.onLoad({ filter: /\.[jt]sx?/ }, async (args) => {
+                        // This actually works for dependencies as well!!
+                        const result = await swc.transformFile(args.path, {
+                            jsc: {
+                                externalHelpers: true,
+                            },
+                            env: {
+                                targets: "defaults",
+                                include: [
+                                    "transform-classes",
+                                    "transform-arrow-functions",
+                                ],
+                            },
+                        });
+                        return { contents: result.code };
+                    });
+                },
+            },
+            alias(aliases),
+            replace({
+                "__vendettaVersion": commit,
+            }),
+        ],
+        legalComments: "none",
     });
-    await bundle.close();
 
     await fs.appendFile("./dist/vendetta.js", "//# sourceURL=Vendetta");
     console.log("Build successful!");
