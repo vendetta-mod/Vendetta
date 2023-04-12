043d8a0507897869866afddc12503deac5a5d40d
Jack Matthews <jm5112356@gmail.com>
2023-04-12 05:10:24 -0400

    [SafeMode] Wrap the error boundary in our own error boundary

--- src/ui/safeMode/index.tsx
+++ src/ui/safeMode/index.tsx
@@ -2,7 +2,7 @@ import { after } from "@lib/patcher";
 import { ReactNative, constants, stylesheet } from "@metro/common";
 import { findByName, findByProps, findByStoreName } from "@metro/filters";
 import { semanticColors } from "@ui/color";
-import { Button } from "@ui/components";
+import { Button, ErrorBoundary as _ErrorBoundary } from "@ui/components";
 
 const ErrorBoundary = findByName("ErrorBoundary");
 
@@ -68,36 +68,37 @@ export default function initSafeMode() {
         if (!this.state.error) return;
 
         return (
-            <SafeAreaView style={styles.container}>
-                <View style={styles.header}>
-                    <Image style={{ flex: 1, resizeMode: "contain", paddingRight: 4 }} source={ThemeStore.theme === "light" ? ret.props.lightSource : ret.props.darkSource} />
-                    <View style={{ flex: 2, paddingLeft: 4 }}>
-                        <Text style={styles.headerTitle}>{ret.props.title}</Text>
-                        <Text style={styles.headerDescription}>{ret.props.body}</Text>
+            <_ErrorBoundary>
+                <SafeAreaView style={styles.container}>
+                    <View style={styles.header}>
+                        <Image style={{ flex: 1, resizeMode: "contain", paddingRight: 4 }} source={ThemeStore.theme === "light" ? ret.props.lightSource : ret.props.darkSource} />
+                        <View style={{ flex: 2, paddingLeft: 4 }}>
+                            <Text style={styles.headerTitle}>{ret.props.title}</Text>
+                            <Text style={styles.headerDescription}>{ret.props.body}</Text>
+                        </View>
                     </View>
-                </View>
-                <View style={styles.main}>
-                    <View style={{ paddingBottom: 8 }}>
-                        {/* Perhaps tabs should be moved out of this, and we should set the default active tab instead of falling back to messages.
+                    <View style={styles.main}>
+                        <View style={{ paddingBottom: 8 }}>
+                            {/* Perhaps tabs should be moved out of this, and we should set the default active tab instead of falling back to messages.
                             Are errors caught by errorboundary guaranteed to have component stack? */}
-                        <BadgableTabBar
-                            tabs={[{ id: "message", title: "Message" }, { id: "stack", title: "Stack Trace" }, { id: "componentStack", title: "Component" }]}
-                            activeTab={this.state.activeTab ?? "message"}
-                            onTabSelected={(tab: string) => { this.setState({ activeTab: tab }) }}
-                        />
+                            <BadgableTabBar
+                                tabs={[{ id: "message", title: "Message" }, { id: "stack", title: "Stack Trace" }, { id: "componentStack", title: "Component" }]}
+                                activeTab={this.state.activeTab ?? "message"}
+                                onTabSelected={(tab: string) => { this.setState({ activeTab: tab }) }}
+                            />
+                        </View>
+                        {/* This is what we must do for text selection on both platforms */}
+                        {ReactNative.Platform.select({
+                            ios: <TextInput editable={false} multiline style={styles.codeBlock} value={this.state.error[this.state.activeTab ?? "message"]} />,
+                            default: <Text selectable style={styles.codeBlock}>{this.state.error[this.state.activeTab ?? "message"]}</Text>,
+                        })}
                     </View>
-                    {/* This is what we must do for text selection on both platforms */}
-                    {ReactNative.Platform.select({
-                        ios: <TextInput editable={false} multiline style={styles.codeBlock} value={this.state.error[this.state.activeTab ?? "message"]} />,
-                        default: <Text selectable style={styles.codeBlock}>{this.state.error[this.state.activeTab ?? "message"]}</Text>,
-                    })}
-                </View>
-                <View style={styles.footer}>
-                    <Button text="Restart Discord" size="small" onPress={this.handleReload} />
-                    {/* <Button style={{ marginTop: 8 }} text="Enter Safe Mode" size="small" color="red" onPress={() => {}}} /> */}
-
-                </View>
-            </SafeAreaView>
+                    <View style={styles.footer}>
+                        <Button text="Restart Discord" size="small" onPress={this.handleReload} />
+                        {/* <Button style={{ marginTop: 8 }} text="Enter Safe Mode" size="small" color="red" onPress={() => {}}} /> */}
+                    </View>
+                </SafeAreaView>
+            </_ErrorBoundary>
         )
     }));
 };
