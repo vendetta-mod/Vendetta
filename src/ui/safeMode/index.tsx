9c6fee02ed75b258fa9d080d8a83f5a27e2b4031
Jack <30497388+FieryFlames@users.noreply.github.com>
2023-04-12 09:20:27 -0400

    [SafeMode] Custom error boundary (#57)
    * [SafeMode] Add mostly complete custom error boundary
    
    * [SafeMode] Use Button from @ui/components in error boundary
    
    * [SafeMode] Wrap the error boundary in our own error boundary
    
    * [SafeMode > ErrorBoundary] Code-style changes
    
    ---------
    
    Co-authored-by: Beef <beefers@riseup.net>

--- src/ui/safeMode/index.tsx
+++ src/ui/safeMode/index.tsx
@@ -1,11 +1,64 @@
-import { findByName } from "@metro/filters";
+import { ReactNative as RN, constants, stylesheet } from "@metro/common";
+import { findByName, findByProps, findByStoreName } from "@metro/filters";
 import { after } from "@lib/patcher";
-import { getAssetIDByName } from "@ui/assets";
-import { ErrorBoundary as _EB } from "@ui/components";
-
-_EB.getDerivedStateFromError
+import { semanticColors } from "@ui/color";
+import { Button, ErrorBoundary as _ErrorBoundary } from "@ui/components";
 
 const ErrorBoundary = findByName("ErrorBoundary");
+
+// React Native's included SafeAreaView only adds padding on iOS.
+const { SafeAreaView } = findByProps("useSafeAreaInsets");
+// Let's just pray they have this.
+const { BadgableTabBar } = findByProps("BadgableTabBar");
+
+const ThemeStore = findByStoreName("ThemeStore");
+
+const { TextStyleSheet } = findByProps("TextStyleSheet");
+const styles = stylesheet.createThemedStyleSheet({
+    container: {
+        flex: 1,
+        backgroundColor: semanticColors.BACKGROUND_PRIMARY,
+    },
+    header: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+    },
+    headerTitle: {
+        ...TextStyleSheet["heading-md/semibold"],
+        textAlign: "center",
+        textTransform: "uppercase",
+        color: semanticColors.HEADER_PRIMARY,
+    },
+    headerDescription: {
+        ...TextStyleSheet["text-sm/medium"],
+        textAlign: "center",
+        color: semanticColors.TEXT_MUTED,
+    },
+    main: {
+        flex: 6,
+        paddingHorizontal: 16,
+    },
+    codeBlock: {
+        flex: 1,
+        fontFamily: constants.Fonts.CODE_SEMIBOLD,
+        fontSize: 12,
+        backgroundColor: semanticColors.BACKGROUND_SECONDARY,
+        color: semanticColors.TEXT_NORMAL,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: semanticColors.BACKGROUND_TERTIARY,
+        padding: 10,
+    },
+    footer: {
+        justifyContent: "flex-end",
+        paddingHorizontal: 32,
+        paddingVertical: 8,
+    },
+});
 
 export default function initSafeMode() {
     const patches = new Array<Function>;
@@ -13,8 +66,38 @@ export default function initSafeMode() {
     patches.push(after("render", ErrorBoundary.prototype, function (this: any, _, ret) {
         if (!this.state.error) return;
 
-        ret.props.darkSource = { uri: "https://cdn.discordapp.com/emojis/1093803211783143504.gif", height: 256, width: 256 };
-        ret.props.title = "Looks like Discord has exploded...";
-        ret.props.body = this.state.error.toString();
+        return (
+            <_ErrorBoundary>
+                <SafeAreaView style={styles.container}>
+                    <RN.View style={styles.header}>
+                        <RN.Image style={{ flex: 1, resizeMode: "contain", paddingRight: 4 }} source={ThemeStore.theme === "light" ? ret.props.lightSource : ret.props.darkSource} />
+                        <RN.View style={{ flex: 2, paddingLeft: 4 }}>
+                            <RN.Text style={styles.headerTitle}>{ret.props.title}</RN.Text>
+                            <RN.Text style={styles.headerDescription}>{ret.props.body}</RN.Text>
+                        </RN.View>
+                    </RN.View>
+                    <RN.View style={styles.main}>
+                        <RN.View style={{ paddingBottom: 8 }}>
+                            {/* Perhaps tabs should be moved out of this, and we should set the default active tab instead of falling back to messages.
+                            Are errors caught by errorboundary guaranteed to have component stack? */}
+                            <BadgableTabBar
+                                tabs={[{ id: "message", title: "Message" }, { id: "stack", title: "Stack Trace" }, { id: "componentStack", title: "Component" }]}
+                                activeTab={this.state.activeTab ?? "message"}
+                                onTabSelected={(tab: string) => { this.setState({ activeTab: tab }) }}
+                            />
+                        </RN.View>
+                        {/* This is what we must do for text selection on both platforms */}
+                        {RN.Platform.select({
+                            ios: <RN.TextInput editable={false} multiline style={styles.codeBlock} value={this.state.error[this.state.activeTab ?? "message"]} />,
+                            default: <RN.Text selectable style={styles.codeBlock}>{this.state.error[this.state.activeTab ?? "message"]}</RN.Text>,
+                        })}
+                    </RN.View>
+                    <RN.View style={styles.footer}>
+                        <Button text="Restart Discord" size="small" onPress={this.handleReload} />
+                        {/* <Button style={{ marginTop: 8 }} text="Enter Safe Mode" size="small" color="red" onPress={() => {}}} /> */}
+                    </RN.View>
+                </SafeAreaView>
+            </_ErrorBoundary>
+        )
     }));
-}
+};
