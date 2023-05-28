d297c54f8ae79597dfb31c76953f7442065d7737
Cynthia Foxwell <gamers@riseup.net>
2023-05-28 10:29:36 -0600

    [Commands] Prevent patch from adding commands to all command types

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -1,30 +1,34 @@
-import { ApplicationCommand } from "@types";
+import { ApplicationCommand, ApplicationCommandType } from "@types";
 import { commands as commandsModule } from "@metro/common";
 import { after } from "@lib/patcher";
 
 let commands: ApplicationCommand[] = [];
 
 export function patchCommands() {
-    const unpatch = after("getBuiltInCommands", commandsModule, (_, res) => res.concat(commands));
+    const unpatch = after("getBuiltInCommands", commandsModule, ([type]: [type: ApplicationCommandType], res: ApplicationCommand[]) => {
+        if (type === ApplicationCommandType.CHAT) return res.concat(commands);
+    });
     return () => {
         commands = [];
         unpatch();
-    }
+    };
 }
 
 export function registerCommand(command: ApplicationCommand): () => void {
-	// Get built in commands
-	const builtInCommands = commandsModule.getBuiltInCommands(1, true, false);
-	builtInCommands.sort(function (a: ApplicationCommand, b: ApplicationCommand) { return parseInt(b.id!) - parseInt(a.id!) });
+    // Get built in commands
+    const builtInCommands = commandsModule.getBuiltInCommands(ApplicationCommandType.CHAT, true, false);
+    builtInCommands.sort(function (a: ApplicationCommand, b: ApplicationCommand) {
+        return parseInt(b.id!) - parseInt(a.id!);
+    });
 
-	const lastCommand = builtInCommands[builtInCommands.length - 1];
+    const lastCommand = builtInCommands[builtInCommands.length - 1];
 
-	// Override the new command's id to the last command id - 1
-	command.id = (parseInt(lastCommand.id, 10) - 1).toString();
+    // Override the new command's id to the last command id - 1
+    command.id = (parseInt(lastCommand.id, 10) - 1).toString();
 
-	// Add it to the commands array
-	commands.push(command);
+    // Add it to the commands array
+    commands.push(command);
 
-	// Return command id so it can be unregistered
-	return () => commands = commands.filter(({ id }) => id !== command.id);
+    // Return command id so it can be unregistered
+    return () => (commands = commands.filter(({ id }) => id !== command.id));
 }
