767b22fc734dbc71bfbc50914486214cfc05af8e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-07-22 21:24:51 +0000

    [UI] Fix You tab settings on 189.4+

--- src/ui/settings/index.ts
+++ src/ui/settings/index.ts
@@ -1,11 +1,10 @@
-import { findByProps } from "@metro/filters";
 import patchPanels from "@ui/settings/patches/panels";
 import patchYou from "@ui/settings/patches/you";
 
 export default function initSettings() {
     const patches = [
         patchPanels(),
-        ...(findByProps("getSettingSearchListItems") ? [patchYou()] : []),
+        ...patchYou(),
     ]
 
     return () => patches.forEach(p => p());
